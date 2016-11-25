93cbe262a8d67dd6a2dc8018ac306d4f2111e5d5
Valeriy <valeriy5543@gmail.com>
2016-11-25 14:34:08 +0200

    VK

---
+++ ValeriyVK.php
@@ -0,0 +1,10 @@
+﻿<?php
+$request = 'http://api.vk.com/method/users.get?uids=id160640085,';
+$response = file_get_contents($request);//зачитываем файл
+$info = array_shift(json_decode($response)->response);//Принимаем закодированную в JSONстроку
+
+//var_dump($info->photo_200); // URL фотографии
+//var_dump($info->status); // Статус
+//var_dump($info->status);
+echo $info->first_name."<br>"; 
+echo $info->last_name; 
